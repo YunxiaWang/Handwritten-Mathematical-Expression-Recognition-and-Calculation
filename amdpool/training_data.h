c2d15a543119b560b41191ba535507c10e64ac56
YunxiaWang <497790284@qq.com>
2015-12-14 13:13:22 -0500

    Update training_data.h

--- amdpool/training_data.h
+++ amdpool/training_data.h
@@ -7,8 +7,9 @@
 #include "ap_int.h"
 
 #define TRAINING_SIZE 2000
+#define TRAINING_SIZE_O 105
 
-const ap_uint<196> training_data[13 * TRAINING_SIZE] = {
+const ap_uint<196> training_data[1 * TRAINING_SIZE] = {
 #include "training_set_0.dat"
 #include "training_set_1.dat"
 #include "training_set_2.dat"
@@ -19,9 +20,10 @@ const ap_uint<196> training_data[13 * TRAINING_SIZE] = {
 #include "training_set_7.dat"
 #include "training_set_8.dat"
 #include "training_set_9.dat"
-#include "training_set_minus.dat"
-#include "training_set_plus.dat"
-#include "training_set_mul.dat"
 };
-
+const ap_uint<196> training_data_O[3 * TRAINING_SIZE_O] = {
+  #include "training_set_minus.dat"
+  #include "training_set_plus.dat"
+  #include "training_set_mul.dat"
+}
 #endif
